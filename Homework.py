ffbab8771ff9412e4dab48295b360aa13648414c
Sugar02 <stasyasakhar@mail.ru>
2022-11-14 00:32:30 +0300

    исправление дз

--- Homework.py
+++ Homework.py
@@ -1,13 +1,13 @@
 string = input()
-test = string.replace(" ", "")
+test = string.replace(" ", "").replace("-",  "")
 if test.isdigit():
     a, b, c = map(float, string.split())
     if a == 0:
         print(-c / b)
     else:
-        D = b ** 2 - 4 * a * c
-        x_1 = (-b + D ** 0.5) / 2 * a
-        x_2 = (-b - D ** 0.5) / 2 * a
+        D = b ** 2 - (4 * a * c)
+        x_1 = (-b + D ** 0.5) / (2 * a)
+        x_2 = (-b - D ** 0.5) / (2 * a)
         print(f'x_1 = {x_1}')
         print(f'x_2 = {x_2}')
 else:
